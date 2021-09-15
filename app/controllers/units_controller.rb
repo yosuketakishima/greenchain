fef334a83a4b46e1b90949c05a02624f9e5ebf28
taki <chainmaker85@gmail.com>
2021-09-15 14:27:57 +0000

    table

---
+++ app/controllers/units_controller.rb
@@ -0,0 +1,20 @@
+class UnitsController < ApplicationController
+    def index
+        @units=Unit.all
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    def results
+    end
+end
