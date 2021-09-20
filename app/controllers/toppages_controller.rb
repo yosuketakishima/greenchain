c3521f2d9baaf769c5cc31e3d06c2b85fe48458b
taki <chainmaker85@gmail.com>
2021-09-20 07:09:51 +0000

    toppage2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,10 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+     @units=current_user.units
+     @temperatures=Temperature.where(unit_id: @units)
+     gon.temperatures=Temperature.all
+    end
+    
   end
 end
