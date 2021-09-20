2814ff23ee9a20b2f43509b4ff61dcf6f6813362
taki <chainmaker85@gmail.com>
2021-09-20 02:29:52 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   get 'toppages/index'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
   
   resources :units, only: [:index, :show, :edit, :update, :destroy]
   get 'units/:id/results', to: 'units#results', as: 'results'
