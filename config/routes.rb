a9e0a7aaa65c916189d3dc542796cdc0d5d2d7f9
taki <chainmaker85@gmail.com>
2021-09-20 03:13:39 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'toppages/index'
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:create]
