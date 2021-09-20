ad9c7a64ce1cd1d9b3e3c0f2d92ce69e47131870
taki <chainmaker85@gmail.com>
2021-09-20 02:07:11 +0000

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'toppages/index'
+  root to: 'toppages#index'
+  
   resources :units, only: [:index, :show, :edit, :update, :destroy]
   get 'units/:id/results', to: 'units#results', as: 'results'
   post 'units/:id/edit', to: 'units#edit', as: 'edit'
